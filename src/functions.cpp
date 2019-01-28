4d4ca66af314dee3187e3f9bb88c3cc68e822211
lerker100 <lerker100@protonmail.com>
2019-01-28 16:27:15 -0500

    working on auto cap flip button

--- src/functions.cpp
+++ src/functions.cpp
@@ -87,7 +87,39 @@ void ShooterOff() {
 
 //
 void Arm() {
-  
+Arm.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+
+      //Resets the arm's starting position for the auto cap flip button if the button is pressed.
+      if (partner.get_digital(DIGITAL_DOWN)) {
+        Arm.set_zero_position(0);
+      }
+
+      if (partner.get_digital(DIGITAL_L1)) {
+        Arm.move(100);
+        pros::delay(300);
+        Arm.move(-100);
+        pros::delay(320);
+        Arm.move(0);
+      }
+      else if (master.get_digital(DIGITAL_L2)) {
+        Arm.move(100);
+        pros::delay(300);
+        Arm.move(-100);
+        pros::delay(320);
+        Arm.move(0);
+      }
+      else {
+        Arm.move(ArmControls);
+      }
+
+      //doesn't let the arm to go over the 18 inches expantion limit unless a button is pressed on the partner controller.
+      if(abs(int(Arm.get_position() >= ArmCeiling && partner.get_digital(DIGITAL_A) == false))) {
+        IsOverHeigh = true;
+        Arm.move(-50);
+      }
+      else {
+        IsOverHeigh = false;
+      }
 }
 
 //Function for the red flag side of the field
