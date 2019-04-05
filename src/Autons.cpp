75cb73b24606b36f4597c38443e3453a4aef5a56
lerker100 <lerker100@pop-os.localdomain>
2019-04-05 17:23:39 -0400

    Finished Red Flag auton.

--- src/Autons.cpp
+++ src/Autons.cpp
@@ -12,19 +12,20 @@ void RedFlag() {
     get the ball from the cap
     Drive Back to the starting wall
   */
-  Intake.move(600);
+  Intake.move(200);
   Drive(3500, 3500, 120);
-
+  Intake.move(0);
+  pros::delay(500);
   FRMotor.move_relative(-3600, 120);
   BRMotor.move_relative(-3600, -120);
   pros::delay(10);
   FLMotor.move_relative(-3600, 120);
   BLMotor.move_relative(-3600, -120);
 
-  while(BottomIntakeSensor.get_value() == 0 && TopIntakeSensor.get_value() == 0) {
-    pros::delay(20);
-  }
-  Intake.move(0);
+  // while(BottomIntakeSensor.get_value() == 0 && TopIntakeSensor.get_value() == 0) {
+  //   pros::delay(20);
+  // }
+  // Intake.move(0);
 
   do {
   pros::delay(20);
@@ -40,7 +41,7 @@ void RedFlag() {
   pros::delay(600);
 
   Drive(200, 200, 30);
-  Rotate(-78, 50);
+  Rotate(-77, 50);
 
 
   /*
@@ -50,14 +51,14 @@ void RedFlag() {
     Shoot the middle flag
     Shooter off
   */
-  Drive(1300, 1300, 100);
+  Drive(1650, 1650, 100);
 
   pros::delay(300);
   Intake.move(100);
-  pros::delay(400);
+  pros::delay(300);
   Intake.move(0);
 
-  Drive(1100, 1100, 100);
+  Drive(1050, 1050, 100);
 
   pros::delay(300);
   Intake.move(100);
@@ -72,13 +73,13 @@ void RedFlag() {
     Turn and hit the bottom flag
     Arm down
   */
-  Rotate(60, 100);
+  Rotate(80, 100);
 
-  Arm.move_relative(1140, 100);
+  Arm.move_relative(1100, 100);
   pros::delay(800);
-  Rotate(55, 50);
+  Rotate(40, 50);
 
-  Arm.move_relative(-1140, 100);
+  Arm.move_relative(-1100, 100);
   pros::delay(500);
 
 
@@ -88,12 +89,12 @@ void RedFlag() {
     Arm up to flip the cap
     Arm up to flag level
   */
-  Rotate(93, 100);
+  Rotate(96, 100);
 
   Drive(-900, -900, 100);
   Arm.move_relative(800, 80);
   pros::delay(500);
-  Arm.move_relative(400, 50);
+  Arm.move_relative(350, 50);
   pros::delay(150);
 
 
@@ -378,7 +379,7 @@ void BlueCap() {
 }
 
 //Veriables and functions for the auton selector.
-int selection = 3;
+int selection = 0;
 const char *titles[] = {"                     Red Flag", "                     Red Cap", "                     Blue Flag", "                     Blue Cap","                   Skills Auton"};
 void (*scripts[])() = {&RedFlag, &RedCap, &BlueFlag, &BlueCap,  &SkillsAuton};
 void LCDScriptExecute() { scripts[selection](); }
